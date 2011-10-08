davidar/path64-compiler

commit 133d586e7ea3eaaaef7a17a1db156341d74859bb

Add support for C_FUNLOC() intrinsic

src/crayf90/fe90/s_intrin.c
@@ -5898,7 +5898,6 @@ c_loc_iso_arg_check(int attr_idx, int info_idx) {
 |*      Function    CLOC(C) intrinsic.                                        *|
 |*      Function    C_LOC(X) intrinsic (traditional Cray).                    *|
 |*      Function    C_LOC(X) intrinsic (iso_c_binding).		              *|
-|*      Function    C_FUNLOC(X) intrinsic.                                    *|
 |*                                                                            *|
 |* Input parameters:                                                          *|
 |*      NONE                                                                  *|
@@ -5932,12 +5931,10 @@ void    loc_intrinsic(opnd_type     *result_opnd,
    info_idx1 = IL_ARG_DESC_IDX(list_idx1);
 #ifdef KEY /* Bug 14150 */
    intrinsic_type which = ATP_INTRIN_ENUM(*spec_idx);
-   if (which == C_Loc_Iso_Intrinsic || which == C_Funloc_Intrinsic) {
+   if (which != C_Loc_Iso_Intrinsic)
      /* Type is already set correctly */
-   }
-   else
 #endif /* KEY Bug 14150 */
-   ATD_TYPE_IDX(ATP_RSLT_IDX(*spec_idx)) = CRI_Ptr_8;
+       ATD_TYPE_IDX(ATP_RSLT_IDX(*spec_idx)) = CRI_Ptr_8;
 
    if (ATP_INTRIN_ENUM(*spec_idx) == Cloc_Intrinsic) {
       ATD_TYPE_IDX(ATP_RSLT_IDX(*spec_idx)) = CRI_Ch_Ptr_8;
@@ -6010,20 +6007,17 @@ void    loc_intrinsic(opnd_type     *result_opnd,
       attr_idx = find_base_attr(&IL_OPND(list_idx1), &unused1, &unused2);
 
 #ifdef KEY /* Bug 14150 */
-      intrinsic_type which_intrinsic = ATP_INTRIN_ENUM(*spec_idx);
-      if (which_intrinsic == C_Loc_Iso_Intrinsic ||
-	which_intrinsic == C_Funloc_Intrinsic) {
-	int found_error = (which_intrinsic == C_Loc_Iso_Intrinsic) ?
-	  c_loc_iso_arg_check(attr_idx, info_idx1) :
-	  (AT_BIND_ATTR(attr_idx) ? 0 : 1692);
-	if (found_error) {
-	  PRINTMSG(arg_info_list[info_idx1].line, found_error, Error,
-	    arg_info_list[info_idx1].col, AT_OBJ_NAME_PTR(*spec_idx));
-	}
-	/* For now, call external procedure because giving Loc_Opr a result
-	 * type of type(c_ptr) or type(c_funptr) blows up elsewhere in the
-	 * front end. Sigh. See also table entry in p_driver.c */
-	goto EXIT;
+      if (ATP_INTRIN_ENUM(*spec_idx) == C_Loc_Iso_Intrinsic) {
+	  int found_error = c_loc_iso_arg_check(attr_idx, info_idx1);
+	  if (found_error) {
+	      PRINTMSG(arg_info_list[info_idx1].line, found_error, Error,
+		       arg_info_list[info_idx1].col, AT_OBJ_NAME_PTR(*spec_idx));
+
+	/* For now, call external procedure because giving Loc_Opr a
+	 * result type of type(c_ptr) blows up elsewhere in the front
+	 * end. Sigh. See also table entry in p_driver.c */
+	      goto EXIT;
+	  }
       }
 #endif /* KEY Bug 14150 */
 
@@ -6085,6 +6079,57 @@ compare_length(opnd_type shape, int rank2) {
   }
   return compare_cn_and_value(OPND_IDX(shape), rank2, Eq_Opr);
 }
+
+
+/******************************************************************************\
+|*                                                                            *|
+|* Description:                                                               *|
+|*      Function    C_FUNLOC(X) intrinsic.                                    *|
+|*                                                                            *|
+|* Input parameters:                                                          *|
+|*      NONE                                                                  *|
+|*                                                                            *|
+|* Output parameters:                                                         *|
+|*      NONE                                                                  *|
+|*                                                                            *|
+|* Returns:                                                                   *|
+|*      NOTHING                                                               *|
+|*                                                                            *|
+\******************************************************************************/
+
+void c_funloc_intrinsic(opnd_type     *result_opnd,
+			expr_arg_type *res_exp_desc,
+			int           *spec_idx) {
+    int line, col, ir_idx;
+    opnd_type x;
+
+    ir_idx = OPND_IDX((*result_opnd));
+
+    line = IR_LINE_NUM(ir_idx);
+    col  = IR_COL_NUM(ir_idx);
+
+    x = IL_OPND(IL_ARG_DESC_IDX(IR_IDX_R(ir_idx)));
+
+    if (!pp_operand(&x) &&
+	(x.fld != AT_Tbl_Idx || AT_OBJ_CLASS(x.idx) != Pgm_Unit)) {
+
+	PRINTMSG(line, 700, Error, col);
+    }
+    else {
+	IR_TYPE_IDX(ir_idx) = ATD_TYPE_IDX(ATP_RSLT_IDX(IR_IDX_L(ir_idx)));
+
+	IR_OPR(ir_idx) = ProcPtr_Opr;
+	IR_OPND_L(ir_idx) = x;
+
+	IR_FLD_R(ir_idx) = 0;
+	IR_IDX_R(ir_idx) = 0;
+
+	res_exp_desc->type = Structure;
+	res_exp_desc->linear_type = Structure_Type;
+	res_exp_desc->type_idx = IR_TYPE_IDX(ir_idx);
+    }
+}
+
 
 
 /******************************************************************************\
@@ -6211,7 +6256,6 @@ void    c_f_procpointer_intrinsic(opnd_type *result_opnd,
 {
     int ir_idx, type_idx, attr;
     int list_idx1, list_idx2;
-    int info_idx1, info_idx2;
     opnd_type fptr, cptr;
     int line, col;
 
@@ -6223,11 +6267,8 @@ void    c_f_procpointer_intrinsic(opnd_type *result_opnd,
     list_idx1 = IR_IDX_R(ir_idx);
     list_idx2 = IL_NEXT_LIST_IDX(list_idx1);
 
-    info_idx1 = IL_ARG_DESC_IDX(list_idx1);
-    info_idx2 = IL_ARG_DESC_IDX(list_idx2);
-
-    cptr = IL_OPND(info_idx1);
-    fptr = IL_OPND(info_idx2);
+    cptr = IL_OPND(list_idx1);
+    fptr = IL_OPND(list_idx2);
 
     if (!pp_operand(&fptr))
 	goto error;
@@ -6262,9 +6303,6 @@ void    c_f_procpointer_intrinsic(opnd_type *result_opnd,
  error:
     PRINTMSG(line, 700, Error, col);
 }
-
-
-
 
 
 /******************************************************************************\

src/crayf90/fe90/debug.h
@@ -1401,6 +1401,7 @@ char		*operator_str[]		= {
 		 	"Cselect_Opr",
 #endif /* KEY Bug 10410 */
 			"ProcPtr_Asg_Opr",
+			"ProcPtr_Opr",
                         "The_Last_Opr"
 		};
 

src/crayf90/fe90/globals.h
@@ -1542,6 +1542,7 @@ enum    operator_values      {  Null_Opr,
 				Cselect_Opr,
 #endif /* KEY Bug 10410 */
 				ProcPtr_Asg_Opr,
+				ProcPtr_Opr,
                                 /* PLACE NEW OPERATORS ABOVE THIS LINE. */
                                 /* DO NOT PUT ANY OPRS AFTER THIS ONE */
 				The_Last_Opr
@@ -2712,6 +2713,7 @@ extern  void  lge_intrinsic     (opnd_type *, expr_arg_type *, int *);
 extern  void  numarg_intrinsic  (opnd_type *, expr_arg_type *, int *);
 extern  void  fcd_intrinsic     (opnd_type *, expr_arg_type *, int *);
 extern  void  loc_intrinsic     (opnd_type *, expr_arg_type *, int *);
+extern  void  c_funloc_intrinsic(opnd_type *, expr_arg_type *, int *);
 extern  void  clock_intrinsic   (opnd_type *, expr_arg_type *, int *);
 
 #ifdef KEY /* Bug 14150 */

src/crayf90/fe90/i_cvrt.c
@@ -3362,6 +3362,7 @@ static void	cvrt_exp_to_pdg(int         ir_idx,
    case Leadz_Opr :
    case Poppar_Opr :
    case Popcnt_Opr :
+   case ProcPtr_Opr :
    case Nint_Opr :
    case Anint_Opr :
    case Sign_Opr :
@@ -3526,6 +3527,7 @@ static void	cvrt_exp_to_pdg(int         ir_idx,
 
         case Int_Opr :
         case Logical_Opr :
+	case ProcPtr_Opr :
         case Real_Opr :
         case Ichar_Opr :
         case Char_Opr :

src/crayf90/fe90/intrin.h
@@ -14196,7 +14196,7 @@ void	(*intrinsic_semantics[]) ()	= {
 #ifdef KEY /* Bug 14150 */
         c_f_pointer_intrinsic,		/*  C_F_Pointer_Intrinsic	     */
         c_f_procpointer_intrinsic,	/*  C_F_Procpointer_Intrinsic	     */
-        loc_intrinsic,			/*  C_Funloc_Intrinsic		     */
+	c_funloc_intrinsic,		/*  C_Funloc_Intrinsic		     */
         loc_intrinsic,			/*  C_Loc_Iso_Intrinsic	             */
 #endif /* KEY Bug 14150 */
 	loc_intrinsic,         		/*  C_Loc_Intrinsic          	     */

src/crayf90/fe90/p_driver.c
@@ -579,7 +579,7 @@ static imt_entry intrinsic_module_table[] = {
   /* For now, call external procedure for C_FUNLOC and C_LOC because front
    * end blows up if loc_intrinsic() in s_intrin.c creates a Loc_Opr whose
    * result is a derived type. */
-  { "C_FUNLOC",				imt_extern, C_Funloc_Intrinsic},
+  { "C_FUNLOC",				imt_inline, C_Funloc_Intrinsic},
   { "C_FUNPTR",				imt_type,   Unknown_Intrinsic},
   { "C_F_POINTERA",			imt_extern, C_F_Pointer_Intrinsic},
   { "C_F_POINTERS",			imt_extern, C_F_Pointer_Intrinsic},
@@ -912,15 +912,14 @@ int intrinsic_module_lookup(int attr_idx)
 	*(dot + 1) = toupper(*dot);
 	*user_name = '_';
       }
-      /* For x8664 -m32, types C_PTR and C_FUNPTR must be treated as scalars
-       * compatible with C "void *", not as structures. In user code, they
-       * are marked with AT_IS_INTRIN: the c_ptr_abi_trouble() function
-       * then ensures that functions which return them treat them as scalars.
-       * But when compiling iso_c_binding.F90, they are not marked specially,
-       * so here we must treat specially the two functions C_LOC and C_FUNLOC
-       * which return them. */
-      if (is_x8664_n32() && (keyp->index == C_Loc_Iso_Intrinsic ||
-	keyp->index == C_Funloc_Intrinsic)) {
+      /* For x8664 -m32, C_PTR must be treated as a scalars compatible
+       * with C "void *", not as a structures. In user code, it is
+       * marked with AT_IS_INTRIN: the c_ptr_abi_trouble() function
+       * then ensures that functions which returns it treat it as a
+       * scalar.  But when compiling iso_c_binding.F90, it is not
+       * marked specially, so here we must treat specially the
+       * function C_LOC which returns it. */
+      if (is_x8664_n32() && keyp->index == C_Loc_Iso_Intrinsic) {
 	ATP_EXTRA_DARG(spec_idx) = FALSE;
 	ATP_NUM_DARGS(spec_idx) = 1;
 	ATP_FIRST_IDX(spec_idx) += 1;

src/libfi/mathlb/iso_c_binding.F90
@@ -127,14 +127,10 @@ module iso_c_binding
     end subroutine c_f_procpointer
   end interface
 
-  ! 15.1.2.4 c_funloc not completely implementable till we have procedure
-  ! pointers; requires type checking in front end code in s_intrin.c
-
   interface c_funloc
     type(c_funptr) function c_funloc(x)
       import :: c_funptr
-      external :: x
-!dir$ ignore_tkr x
+      procedure (), pointer :: x
     end function c_funloc
   end interface c_funloc
 

commit 9c563812e5ef7b775fac3eceecaf049d98b66f13

COMPILER-9036
Fix bug introduced by commit 133d586e7ea3eaaaef7a17a1db156341d74859bb

src/crayf90/fe90/s_intrin.c
@@ -6013,11 +6013,11 @@ void    loc_intrinsic(opnd_type     *result_opnd,
 	      PRINTMSG(arg_info_list[info_idx1].line, found_error, Error,
 		       arg_info_list[info_idx1].col, AT_OBJ_NAME_PTR(*spec_idx));
 
+	  }
 	/* For now, call external procedure because giving Loc_Opr a
 	 * result type of type(c_ptr) blows up elsewhere in the front
 	 * end. Sigh. See also table entry in p_driver.c */
-	      goto EXIT;
-	  }
+	  goto EXIT;
       }
 #endif /* KEY Bug 14150 */
 