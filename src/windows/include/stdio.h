eec902494f19c1b833076230d21f10b9b3312d5e
Ryan Pavlik <rpavlik@iastate.edu>
2011-10-11 15:55:32 -0500

    OK, so somewhere in the middle: the mingw64 build doesn't need stdio.h override

--- src/windows/include/stdio.h
+++ src/windows/include/stdio.h
@@ -1,6 +1,9 @@
 
 #include_next <stdio.h>
 
+#ifndef __MINGW64__
+/* mingw w64 runtime conflicts with this, but not mingw32. */
+
 #ifndef __STDIO_PROXY_H_
 #define __STDIO_PROXY_H_
 
@@ -18,3 +21,5 @@ int asprintf(char **strp, const char *fmt, ...);
 #endif
 
 #endif /* __STDIO_PROXY_H_ */
+
+#endif
