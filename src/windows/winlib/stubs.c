9ab2078fb40c04ee149bed13f136df51e0d075a1
Ryan Pavlik <rpavlik@iastate.edu>
2011-10-11 15:46:57 -0500

    remove outdated windows stubs

--- src/windows/winlib/stubs.c
+++ src/windows/winlib/stubs.c
@@ -29,12 +29,6 @@
 #include <sys/resource.h>
 #include <windows.h>
 
-const char * Anl_File_Path()
-{
-    fprintf(stderr, "Anl_File_Path() is unimplemented\n");
-    return 0;
-}
-
 int getrusage(int who, struct rusage *usage)
 {
     FILETIME CreateTime, ExitTime;
@@ -76,20 +70,6 @@ char *sbrk(int increment)
     return 0;
 }
 
-__int64 
-Get_Next_Construct_Id()
-{
-    fprintf(stderr, "Get_Next_Construct_Id() is unimplemented\n");
-    return 0;
-}
-
-__int64 
-New_Construct_Id()
-{
-    fprintf(stderr, "New_Construct_Id() is unimplemented\n");
-    return 0;
-}
-
 int kill(pid_t pid, int sig)
 {
     fprintf(stderr, "kill() is unimplemented\n");
@@ -109,42 +89,6 @@ int getdomainname(char *name, size_t len)
     fprintf(stderr, "getdomainname() is unimplemented\n");
     snprintf(name, len, "fakedomain");
     return 0;
-}
-
-void Anl_Process_Command_Line(int phase_argc, char *phase_argv[], int argc, char *argv[])
-{
-    fprintf(stderr, "Anl_Process_Command_Line() is unimplemented\n");
-}
-
-int Anl_Needs_Whirl2c(void)
-{
-    return 0;
-}
-
-int Anl_Needs_Whirl2f(void)
-{
-    return 0;
-}
-
-void Anl_Init(void)
-{
-    fprintf(stderr, "Anl_Init() is unimplemented\n");
-}
-
-int Anl_Init_Map(int *id_map_pool)
-{
-    fprintf(stderr, "Anl_Init_Map() is unimplemented\n");
-    return 0;
-}
-
-void Anl_Fini(void)
-{
-    fprintf(stderr, "Anl_Fini() is unimplemented\n");
-}
-
-void Anl_Static_Analysis(int *pu, int id_map)
-{
-    fprintf(stderr, "Anl_Fini() is unimplemented\n");
 }
 
 #if 0
